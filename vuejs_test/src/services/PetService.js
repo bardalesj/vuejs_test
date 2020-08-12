fb91e01ecd8cd7e2210402be118e31f2e12e6757
José Bardales <57644515+bardalesj@users.noreply.github.com>
2020-08-12 16:06:46 -0600

    Comentarios

--- vuejs_test/src/services/PetService.js
+++ vuejs_test/src/services/PetService.js
@@ -5,18 +5,23 @@ const params = {
 	token: "e2990f099f38288eb9d8d20762188b84",
 };
 class PetService{
+	//Metodo para obtener toda la informacion
   retrieveAllPets(){
     return axios.get(`${url}`,{params});
   }
+	//Metodo para obtener toda la informacion por id
 	retrievePet(id) {
     return axios.get(`${url}/${id}`,{params});
 }
+	//Metodo para agregar nueva informacion
 	addPet(id, pet) {
 			return axios.post(`${url}`,pet,{params});
 	}
+	//Metodo para modificar informacion
 	updatePet(id, pet) {
       return axios.put(`${url}/${id}`,pet,{params});
   }
+	//Metodo para eliminar informacion
 	deletePet(id){
 		return axios.delete(`${url}/${id}?token=${token}`);
 	}
